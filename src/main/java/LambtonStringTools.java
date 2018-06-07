6b7409b3929009d723583368178a3bb583028746
user <ravneets47@gmail.com>
2018-06-07 19:14:11 -0400

    Reverse function created

---
+++ src/main/java/LambtonStringTools.java
@@ -0,0 +1,26 @@
+public class LambtonStringTools {
+
+    private String s1 = "Lambton";;
+    private char c[] = s1.toCharArray();
+    private String s2;
+
+    public LambtonStringTools(){}
+
+//    public LambtonStringTools(String s) {
+//        this.s1
+//    }
+
+    public void reverse()
+    {
+        for (int i = c.length-1; i >= 0; i--)
+        {
+            System.out.print(c[i]);
+
+        }
+
+    }
+    public void binaryToDecimal()
+    {
+
+    }
+}
