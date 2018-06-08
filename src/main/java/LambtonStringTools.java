af47798ae6d59e7a15e673ee9a9ba80f1d895652
user <ravneets47@gmail.com>
2018-06-07 20:43:57 -0400

    other functions created

--- src/main/java/LambtonStringTools.java
+++ src/main/java/LambtonStringTools.java
@@ -1,8 +1,6 @@
 public class LambtonStringTools {
 
 
-    private String s2;
-
     public LambtonStringTools(){}
 
     public String reverse(String s)
@@ -13,13 +11,67 @@ public class LambtonStringTools {
         {
             System.out.print(c[i]);
         }
+
         return null;
     }
-    /*public int binaryToDecimal(String s)
+    public int binaryToDecimal(String s)
+    {
+        double a = 0;
+        int b = 0;
+        a = Double.parseDouble("s");
+
+
+       return b;
+    }
+
+    public void initials(String s)
+    {
+       // String text = "this is a.line is. over";
+
+        int a = 0;
+        boolean capitalize = true;
+        StringBuilder sb = new StringBuilder(s);
+        while (a < sb.length()) {
+            if (sb.charAt(a) == '.') {
+                capitalize = true;
+            } else if (capitalize && !Character.isWhitespace(sb.charAt(a))) {
+                sb.setCharAt(a, Character.toUpperCase(sb.charAt(a)));
+                capitalize = false;
+            }
+            a++;
+        }
+        System.out.println(sb.toString());
+    }
+//        char c[] = s.toCharArray();
+//        if(c.)
+//        {
+//            System.out.println(c[0].);
+//        }
+//    }
+
+    public char mostFrequent(String s)
     {
 
+        char c;
+        char c1[] = s.toCharArray();
+        for (int i = 0; i < c1.length-1; i++) {
+
+            c= c1[i] ;
+           // if(c1[i] = c1[i+1])
+            {
+
+            }
+
+        }
 
 
-       return
-    }*/
+        return c;
+    }
+
+
+    public String replaceSubString(String s1, String s2, String s3)
+    {
+
+        return null;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ public class Main
 {
     public static void main(String args[])
     {
+        LambtonStringTools first = new LambtonStringTools();
+        first.reverse("Lambton");
 
+        LambtonStringTools fourth = new LambtonStringTools();
+        fourth.initials(".James .tiBeriUs .kiRK");
     }
 }
